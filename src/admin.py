d8a85085ba6e01ea0c813616d45d6d83ea6b2c87
mahsa <mahsadibajii@gmail.com>
2020-03-25 01:28:15 -0700

    ADD Department and Professor models & extend User

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from src.models import Department, Professor, User
+
+admin.site.register(Department)
+admin.site.register(Professor)
+admin.site.register(User)
--- src/models.py
+++ src/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=128)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"professor: {self.name} - dep:{self.department}"
+
+
+class User(AbstractUser):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
