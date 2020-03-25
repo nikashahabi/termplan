0dbb3b5d469d54ce6dc4784b285df28d5e051be9
parnian taghipour <parniantaghipour@gmail.com>
2020-03-25 13:52:51 +0430

    adding model courses

--- src/admin.py
+++ src/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from src.models import Department, Professor, User
+from src.models import Department, Professor, User, Courses
 
 admin.site.register(Department)
 admin.site.register(Professor)
 admin.site.register(User)
+admin.site.register(Courses)
--- src/models.py
+++ src/models.py
@@ -19,3 +19,21 @@ class Professor(models.Model):
 
 class User(AbstractUser):
     department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Courses(models.Model):
+    name: models.CharField(max_length=256)
+    start_time: models.TimeField(null=True, blank=True)
+    end_time: models.TimeField(null=True, blank=True)
+    Day1: models.CharField(max_length=256, null=True, blank=True)
+    Day2: models.CharField(max_length=256, null=True, blank=True)
+    unit: models.IntegerField(null=True, blank=True)
+    exam: models.CharField(max_length=256, null=True, blank=True)
+    ta_day: models.CharField(max_length=256, null=True, blank=True)
+    ta_time: models.TimeField(null=True, blank=True)
+    semester: models.CharField(max_length=256, null=True, blank=True)
+    department: models.ForeignKey(Department, on_delete=models.CASCADE)
+    professor:  models.ForeignKey(Professor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"course:{self.name} - dep:{self.department}"
