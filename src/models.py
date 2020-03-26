9ff90cf1a19fde73c4b33e518e642da506bdccae
parnian taghipour <parniantaghipour@gmail.com>
2020-03-26 18:12:22 +0430

    the problems solved

--- src/models.py
+++ src/models.py
@@ -22,18 +22,18 @@ class User(AbstractUser):
 
 
 class Courses(models.Model):
-    name: models.CharField(max_length=256)
-    start_time: models.TimeField(null=True, blank=True)
-    end_time: models.TimeField(null=True, blank=True)
-    Day1: models.CharField(max_length=256, null=True, blank=True)
-    Day2: models.CharField(max_length=256, null=True, blank=True)
-    unit: models.IntegerField(null=True, blank=True)
-    exam: models.CharField(max_length=256, null=True, blank=True)
-    ta_day: models.CharField(max_length=256, null=True, blank=True)
-    ta_time: models.TimeField(null=True, blank=True)
-    semester: models.CharField(max_length=256, null=True, blank=True)
-    department: models.ForeignKey(Department, on_delete=models.CASCADE)
-    professor:  models.ForeignKey(Professor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    Day1 = models.CharField(max_length=256, null=True, blank=True)
+    Day2 = models.CharField(max_length=256, null=True, blank=True)
+    unit = models.IntegerField(null=True, blank=True)
+    exam = models.DateTimeField( null=True, blank=True)
+    ta_day = models.CharField(max_length=256, null=True, blank=True)
+    ta_time = models.TimeField(null=True, blank=True)
+    semester = models.CharField(max_length=256, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"course:{self.name} - dep:{self.department}"
