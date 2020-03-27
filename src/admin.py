e5a0a5dd98df5783d2331dfb2d6146f95989bbca
mahsa <mahsadibajii@gmail.com>
2020-03-27 09:32:48 -0700

    ADD models course, semester course, user course

--- src/admin.py
+++ src/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from src.models import Department, Professor, User, Courses
+from src.models import Department, Professor, User, Course, SemesterCourse, UserCourse
 
 admin.site.register(Department)
 admin.site.register(Professor)
 admin.site.register(User)
-admin.site.register(Courses)
+admin.site.register(Course)
+admin.site.register(SemesterCourse)
+admin.site.register(UserCourse)
