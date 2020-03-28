7802c4456dfa981b53bb16030d283597dfa45eb9
parnian taghipour <parniantaghipour@gmail.com>
2020-03-28 15:24:57 +0430

    delete course api added

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from src.views import list_course, add_course
+from src.views import list_course, add_course, course_delete
 
 urlpatterns = [
     path('courses_list', list_course),
-    path('add_course', add_course)
+    path('add_course', add_course),
+    path('del_course', course_delete)
 ]
--- src/views.py
+++ src/views.py
@@ -45,7 +45,7 @@ def add_course(request):
     overlapping_course = user_semester.courses.filter(Q(day1=wanted_course.day1) | Q(day2=wanted_course.day2), Q(
         start_time__range=(wanted_course.start_time, wanted_course.end_time)) | Q(
         end_time__range=(wanted_course.start_time, wanted_course.end_time))).exclude(
-        Q(start_time=wanted_course.end_time)|Q(end_time=wanted_course.start_time))
+        Q(start_time=wanted_course.end_time) | Q(end_time=wanted_course.start_time))
     if overlapping_course.exists():
         return JsonResponse({
             "error": "تداخل دارد",
@@ -53,3 +53,19 @@ def add_course(request):
         })
     user_semester.courses.add(wanted_course)
     return JsonResponse({"message": "با موفقیت اضاقه شد"})
+
+
+@csrf_exempt
+def course_delete(request):
+    data = json.loads(request.body)
+    username = data.get("username")
+    user = User.objects.filter(username=username).first()
+    semester = data.get("semester")
+    course_name = data.get("course")
+    selected_course = SemesterCourse.objects.filter(course__name=course_name, semester=semester).first()
+    user_semester, _ = UserCourse.objects.get_or_create(user=user, semester=semester)
+    if user_semester.courses.filter(course=selected_course).exists():
+        user_semester.courses.remove(selected_course)
+        return JsonResponse({"message": "با موفقیت حذف شد"})
+    else:
+        return JsonResponse({"message": "درس مورد نظر یافت نشد"})
