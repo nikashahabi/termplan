43ad129d061c0a87c59d1ec2c87d560f7f4ca10b
mahsa dibaji <mahsadibajii@gmail.com>
2020-04-11 22:01:04 +0430

    Fix apis to be proper for front

--- src/views.py
+++ src/views.py
@@ -1,6 +1,5 @@
 import json
 
-from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -16,19 +15,25 @@ def courses_list(request):
     courses = []
     data_of_db = SemesterCourse.objects.filter(semester__exact=semester, course__department_id=department_id)
     for data in data_of_db:
+        days = [data.day1, data.day2]
         courses.append({
+            "course_id": f"{data.course.code}-{data.group}",
             "name": data.course.name,
-            "start_time": data.start_time,
-            "end_time": data.end_time,
-            "day1": data.Day1,
-            "day2": data.Day2,
+            "class_times": [{
+                "day": day,
+                "start": data.start_time,
+                "end": data.end_time
+            } for day in days if day],
             "ta_time": data.ta_time,
             "ta_day": data.ta_day,
-            "exam": data.exam,
+            "exam_time": data.exam,
             "department": data.course.department.name,
-            "unit": data.course.unit,
-            "prof": data.professor.name,
-            "semester": data.semester
+            "units": data.course.unit,
+            "instructor": data.professor.name,
+            "semester": data.semester,
+            "course_number": data.course.code,
+            "info": data.info,
+            "capacity": data.capacity
 
         })
     return JsonResponse({"data": courses})
@@ -37,35 +42,40 @@ def courses_list(request):
 @csrf_exempt
 def add_course(request):
     data = json.loads(request.body)
-    username = data.get("username")
-    user = User.objects.filter(username=username).first()
-    course_name = data.get("course")
+    # username = data.get("username")
+    user = User.objects.filter(username="temp").first()
+    course_id = data.get("course_id")
+    course_code, course_group = course_id.split("-")
+    print(course_code, course_group)
     semester = data.get("semester")
-    wanted_course = SemesterCourse.objects.filter(course__name=course_name, semester=semester).first()
+    selected_course = SemesterCourse.objects.filter(course__code=course_code, group=course_group,
+                                                    semester=semester).first()
     user_semester, _ = UserCourse.objects.get_or_create(user=user, semester=semester)
-    overlapping_course = user_semester.courses.filter(Q(day1=wanted_course.day1) | Q(day2=wanted_course.day2), Q(
-        start_time__range=(wanted_course.start_time, wanted_course.end_time)) | Q(
-        end_time__range=(wanted_course.start_time, wanted_course.end_time))).exclude(
-        Q(start_time=wanted_course.end_time) | Q(end_time=wanted_course.start_time))
-    if overlapping_course.exists():
-        return JsonResponse({
-            "error": "تداخل دارد",
-            "overlapping course": overlapping_course.first().course.name
-        })
-    user_semester.courses.add(wanted_course)
+    # overlapping_course = user_semester.courses.filter(Q(day1=wanted_course.day1) | Q(day2=wanted_course.day2), Q(
+    #     start_time__range=(wanted_course.start_time, wanted_course.end_time)) | Q(
+    #     end_time__range=(wanted_course.start_time, wanted_course.end_time))).exclude(
+    #     Q(start_time=wanted_course.end_time) | Q(end_time=wanted_course.start_time))
+    # if overlapping_course.exists():
+    #     return JsonResponse({
+    #         "error": "تداخل دارد",
+    #         "overlapping course": overlapping_course.first().course.name
+    #     })
+    user_semester.courses.add(selected_course)
     return JsonResponse({"message": "با موفقیت اضاقه شد"})
 
 
 @csrf_exempt
 def delete_course(request):
     data = json.loads(request.body)
-    username = data.get("username")
-    user = User.objects.filter(username=username).first()
+    # username = data.get("username")
     semester = data.get("semester")
-    course_name = data.get("course")
-    selected_course = SemesterCourse.objects.filter(course__name=course_name, semester=semester).first()
+    course_id = data.get("course_id")
+    user = User.objects.filter(username="temp").first()  # temp user for now
+    course_code, course_group = course_id.split("-")
+    selected_course = SemesterCourse.objects.filter(course__code=course_code, group=course_group,
+                                                    semester=semester).first()
     user_semester, _ = UserCourse.objects.get_or_create(user=user, semester=semester)
-    if user_semester.courses.filter(course=selected_course).exists():
+    if selected_course in user_semester.courses.all():
         user_semester.courses.remove(selected_course)
         return JsonResponse({"message": "با موفقیت حذف شد"})
     else:
@@ -73,12 +83,12 @@ def delete_course(request):
 
 
 def schedule(request):
-    # username = request.user.username
+    temp_user, _ = User.objects.get_or_create(username="temp")  # temp user for now
     departments = []
     for dep in Department.objects.all():
         departments.append({"name": dep.name, "id": dep.id})
     data = {
-        "username": "test username",
+        "username": temp_user.username,
         "departments": departments
     }
     return render(request, 'grid.html', data)
