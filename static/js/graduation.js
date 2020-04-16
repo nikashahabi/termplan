9df615a67554d7ebea203638893d7315ed01023c
behrad khorram <behrrad@aut.ac.ir>
2020-04-17 02:56:48 +0430

    get courses request added

--- static/js/graduation.js
+++ static/js/graduation.js
@@ -1,3 +1,4 @@
+var get_courses = new Array();
 function reply_click(clicked_id){
     
     var button = document.getElementById(clicked_id);
@@ -9,4 +10,26 @@ function reply_click(clicked_id){
         add_course(clicked_id);
         panel.style.display = "block";
     }
+}
+
+function add_course(groupId){
+    for(var u in get_courses) {
+        if(u == groupId)
+            return;
+    }
+    get_courses[groupId] = groupId;
+    $.ajax({
+           url: '/courses_list_group',
+           type: 'post',
+           data: { group: groupId},
+           success: function(data){
+           var courses = data.data;
+           for(i = 0; i < courses.length; i++) {
+           $("#course-group-" + groupId).append(sprintf('<li><input type="checkbox" name="chk" id="%s">%s</input></li>', courses[i].id, courses[i].name));
+           $("#course-group-" + groupId + " li:last").data('course', courses[i]);
+           }
+           },
+           });
+    
+    
 }
