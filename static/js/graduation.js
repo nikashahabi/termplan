ba263f35f3d5ecdb8cef3b6ef5560699f5e245b7
behrad khorram <behrrad@aut.ac.ir>
2020-04-17 02:52:30 +0430

    click function fixed

--- static/js/graduation.js
+++ static/js/graduation.js
@@ -1,14 +1,12 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+function reply_click(clicked_id){
+    
+    var button = document.getElementById(clicked_id);
+    button.classList.toggle("active");
+    var panel = button.nextElementSibling;
     if (panel.style.display === "block") {
-      panel.style.display = "none";
+        panel.style.display = "none";
     } else {
-      panel.style.display = "block";
+        add_course(clicked_id);
+        panel.style.display = "block";
     }
-  });
 }
