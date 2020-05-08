5244d1d9ae95bb105e18a4773b7c4da03a72fe9d
mahsa dibaji <mahsadibajii@gmail.com>
2020-05-08 12:14:34 +0430

    BugFix STATIC_ROOT

--- terminator/settings.py
+++ terminator/settings.py
@@ -117,6 +117,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics.py')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
 AUTH_USER_MODEL = 'src.User'
