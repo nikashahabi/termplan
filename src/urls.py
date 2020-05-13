25cabc637362ea39b9548ba91e0231958b78c260
mahsa dibaji <mahsadibajii@gmail.com>
2020-05-13 17:18:26 +0430

    ADD url for show_remained api

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 
-from src.views import courses_list, add_course, delete_course, schedule, graduation, add_passed_course
+from src.views import courses_list, add_course, delete_course, schedule, graduation, add_passed_course, show_remained
 
 urlpatterns = [
     re_path(r'^courses_list/?$', courses_list),
@@ -8,7 +8,8 @@ urlpatterns = [
     re_path(r'^delete_course/?$', delete_course),
     re_path(r'^schedule/?$', schedule),
     re_path(r'^graduation/?$', graduation),
-    re_path(r'^add_passed_course/?$', add_passed_course)
+    re_path(r'^add_passed_course/?$', add_passed_course),
+    re_path(r'^remained_courses/?$', show_remained)
     # re_path(r'^course_chart/', course_chart)
 
 ]
