2a860cdc9530c8faf0b120302d97ec33a9307bd9
nika shahabi <nikashahabi@aut.ac.ir>
2020-05-22 23:46:36 +0430

    graduation show result table bugs fixed

--- static/js/graduation.js
+++ static/js/graduation.js
@@ -32,20 +32,38 @@ function submit_button() {
 }
 
 function group_number() {
-
     $.ajax({
         url: '/graduation',
         type: 'post',
         data: JSON.stringify({group: "0", user: "temp"}),
         success: function (data) {
-            group_numbers = data.data;
+            group_numbers = data.group_count;
+            
             create_button(group_numbers);
         },
     });
-
+    show_remained();
 }
-
+function show_remained(){
+    $.ajax({
+        url: '/remained_courses',
+        success: function(data){
+        courses = data.remain;
+        create_table(courses);
+        
+        },
+    });
+}
+function create_table(courses){
+    for (i = 0; i < courses.length; i++){
+        var necessity = "نیست";
+        if(courses[i].necessity)
+            necessity = "هست";
+        $("#remained-unit").append(sprintf('<tr><th>%s</th><th>%s</th><th>%s</th></tr>', courses[i].course_name , courses[i].course_unit, necessity));
+    }
+}
 function create_button(group_numbers) {
+    
     var tedad = parseInt(group_numbers);
     for (i = 0; i < tedad; i++) {
 
