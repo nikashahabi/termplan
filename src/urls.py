c71e6fcc8b8514363864ab376349333f32acf7d0
mahsa dibaji <mahsadibajii@gmail.com>
2020-05-22 17:31:12 +0430

    ADD url for class SemesterCourseAddView(APIView):

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
 
-from src.views import courses_list, add_course, delete_course, schedule, graduation, add_passed_course, show_remained
+from src.views import courses_list, add_course, delete_course, schedule, graduation, add_passed_course, show_remained, \
+    SemesterCourseAddView
 
+app_name = 'src'
 urlpatterns = [
     re_path(r'^courses_list/?$', courses_list),
     re_path(r'^add_course/?$', add_course),
@@ -9,7 +11,8 @@ urlpatterns = [
     re_path(r'^schedule/?$', schedule),
     re_path(r'^graduation/?$', graduation),
     re_path(r'^add_passed_course/?$', add_passed_course),
-    re_path(r'^remained_courses/?$', show_remained)
+    re_path(r'^remained_courses/?$', show_remained),
+    re_path(r'^upload_semester_file/?$', SemesterCourseAddView.as_view(), name='upload_file')
     # re_path(r'^course_chart/', course_chart)
 
 ]
