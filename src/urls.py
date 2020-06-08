d6f6a4ff19ca204fbced096ee5e422a6cff6289e
mahsa dibaji <mahsadibajii@gmail.com>
2020-06-08 17:34:36 +0430

    Bugfix in urls

--- src/urls.py
+++ src/urls.py
@@ -14,6 +14,5 @@ urlpatterns = [
     re_path(r'^remained_courses/?$', show_remained),
     re_path(r'^upload_semester_file/?$', SemesterCourseAddView.as_view(), name='upload_file'),
     re_path(r'^upload_department_file/?$', DepartmentChartAddView.as_view(), name='upload_file')
-    re_path(r'^course_chart/', course_chart)
 
 ]
